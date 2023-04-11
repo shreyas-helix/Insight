5a1498604b93065a46aaa472c7afd4eae2531d50
KV <Shreyas.KV@ellucian.com>
2023-04-11 12:20:08 +0530

    Home Page and SnipRead page done!

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import sum.Summary_page as sp
+import SnipRead.Summary_page as sp
+import QueryPal.Question_page as qp
 from home import homeWork as hW
 
-st.set_page_config(layout="centered",page_title="AI Professor",page_icon="balloon")
+st.set_page_config(layout="wide",page_title="AI Professor",page_icon="balloon")
 
 #if you want a sidebar
-# with st.sidebar:
-#     selected = option_menu(
-#         menu_title="SpeedBar",
-#         options = ["Home",
-#                    "SnipRead",
-#                    "QueryPal"],
-#         menu_icon=["lightning"],
-#         icons= ["house","scissors","robot"],
-#         default_index= 0,
-#     )
-
-# If you want it in middle of the page, uncomment down!
-    
-selected = option_menu(
-        # menu_title="SpeedBar",
-        menu_title=None,
+with st.sidebar:
+    selected = option_menu(
+        menu_title="SpeedBar",
         options = ["Home",
                    "SnipRead",
                    "QueryPal"],
-        # menu_icon=["lightning"],
+        menu_icon=["lightning"],
         icons= ["house","scissors","robot"],
         default_index= 0,
-        orientation= "horizontal"
     )
+
+# If you want it in middle of the page, uncomment down!
+    
+# selected = option_menu(
+#         # menu_title="SpeedBar",
+#         menu_title=None,
+#         options = ["Home",
+#                    "SnipRead",
+#                    "QueryPal"],
+#         # menu_icon=["lightning"],
+#         icons= ["house","scissors","robot"],
+#         default_index= 0,
+#         orientation= "horizontal"
+#     )
 
 if selected == "Home":
     hW()
 if selected == "SnipRead":
-    sp.SnipRead()
+    sp.Snip()
 if selected == "QueryPal":
-    st.title(f"You are in {selected}")
+    qp.Query()
--- home.py
+++ home.py
@@ -11,24 +11,94 @@ def load_lottieurl(url):
 
 #--- Load Assets ---
 lottie_professor = load_lottieurl("https://assets5.lottiefiles.com/private_files/lf30_Fy9W8c.json")
+lottie_SnipRead = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_4D9n2fWE2B.json")
+lottie_QueryPal = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_au4zdsr8.json")
+lottie_SwiftGrade = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_rI27TX7MBp.json")
+
+
 def homeWork():
     with st.container():
         left_column,right_column = st.columns(2)
         with left_column:
             st.write('##')
             st.write('##')
+            st.write('##')
+            st.write('##')
             st.title("Hey Fellas!")
-            st.subheader("I am the AI Professor")
+            st.subheader("Meet the AI Professor")
             ##fill the below function with some suitable short description
             st.write("""
-            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 
-            1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by 
-            English versions from the 1914 translation by H. Rackham.
+            Your personal academic assistant! Summarizes long articles, creates questions, and auto-grades your responses, 
+            making learning simpler and efficient. Get instant feedback and save valuable time with this innovative tool for higher education. 
+            Say goodbye to tedious note-taking and grading, and embrace the future of education with AI Professor.
             """)
         with right_column:
-            st.write("##")
-            st_lottie(lottie_professor, height=400)
+            st.write('##')
+            st_lottie(lottie_professor,height=500)
 
     st.write("---")
 
+    with st.container():
+        st.markdown("""
+        <style>
+        .stCentered {
+            text-align: center;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+        st.write("<h2 class='stCentered'>Tools</h2>", unsafe_allow_html=True)
+        st.markdown("")
+        st.markdown("")
+
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write("##")
+            st.write("<h4>SnipRead</h4>",unsafe_allow_html=True)
+            st.write("""
+            SnipRead is a text summarization tool for higher education that condenses lengthy academic texts into shorter versions using
+            advanced natural language processing techniques.
+            It helps students and researchers quickly grasp the main points of a document, increasing their productivity and efficiency
+            """)
+        with col2:
+            st.write("##")
+            st_lottie(lottie_SnipRead, height=275)
     
+    #st.write('---')
+    
+    with st.container():
+        col3,col4 = st.columns(2)
+
+        with col3:
+            st.write("##")
+            st.write("<h4>QueryPal</h4>",unsafe_allow_html=True)
+            st.write("""
+            QueryPal is an auto question generation tool for higher education that automatically creates questions from a given text or document. 
+            It enables teachers and students to test their understanding of the material and can save time in creating study materials while providing a
+            more comprehensive evaluation of students' knowledge and skills.
+            """)
+        with col4:
+            st.write('##')
+            st_lottie(lottie_QueryPal, height=200)
+
+    #st.write('---')
+    st.markdown('')
+    st.markdown('')
+    st.markdown('')
+    st.markdown('')
+
+    with st.container():
+        col5,col6 = st.columns(2)
+
+        with col5:
+            st.write("##")
+            st.write("<h4>SwiftGrade</h4>",unsafe_allow_html=True)
+            st.write("""
+            SwiftGrade is an auto grading tool for higher education that streamlines the grading process and provides instant feedback to students. 
+            It can save teachers time and effort while also providing students with a more efficient and objective evaluation of their work.
+            """)
+        with col6:
+            
+            st_lottie(lottie_SwiftGrade, height=230)
